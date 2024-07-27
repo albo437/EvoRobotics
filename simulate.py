4581ac26e3fdeaa506776ba19c643467d1cd1b53
Alvaro Bolaños <alvarobolanos@MacBook-Air-270.local>
2024-07-27 13:39:47 -0600

    Load many link file.

--- simulate.py
+++ simulate.py
@@ -8,8 +8,8 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
-for i in range(1000):
+p.loadSDF("boxes.sdf")
+for i in range(500):
     time.sleep(1/60)
     print(i)
     p.stepSimulation()
