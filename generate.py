11dac1da50c2b73b34a56d12f675f02c1221da47
Alvaro Bolaños <alvarobolanos@MacBook-Air-270.local>
2024-07-27 13:38:40 -0600

    Add one more link, prepare for several links.

--- generate.py
+++ generate.py
@@ -1,14 +1,15 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 #Dimensions of the box
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 #Position of the box
 x = 0
 y = 0
-z = 1.5
+z = 0.5
 pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
+pyrosim.Send_Cube(name="Box2", pos=[x, y, z] , size=[length, width, height])
 pyrosim.End()
