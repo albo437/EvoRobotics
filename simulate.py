35b182f8c44544b035e52e6fe6def0f35a2482a9
Alvaro Bolaños <alvarobolanos@MacBook-Air-270.local>
2024-07-26 21:50:29 -0600

    Add enough time to test motion in GUI.

--- simulate.py
+++ simulate.py
@@ -2,9 +2,9 @@ import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)ß
 
-for i in range(100):
+for i in range(1000):
     time.sleep(1/60)
     print(i)
     p.stepSimulation()
