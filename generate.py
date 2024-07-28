f6c267bc285eda66ddd439c0a09ad0f45d7cc970
Alvaro Bolaños <alvarobolanos@MacBook-Air-270.local>
2024-07-27 23:07:08 -0600

    delete tower generation, generate 1 block.

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Start_SDF("world.sdf")
 
 #Dimensions of the box
 length = 1
@@ -10,19 +10,7 @@ height = 1
 x = 0
 y = 0
 z = 0.5
-size_modifier = 1
-name = 0
-for xpos in range(5):
-    for ypos in range(5):
-        for i in range(10):
-            name += 1
-            boxname = "box" + str(name)
-            pyrosim.Send_Cube(boxname, [x, y, z], [length*size_modifier, width*size_modifier, height*size_modifier])
-            z = z + height
-            size_modifier *= 0.9
-        x = x + length
-        z = 0.5
-        size_modifier = 1
-    x = 0
-    y = y + width
+
+pyrosim.Send_Cube(name = "box", pos = [x, y, z], size = [length, width, height])
+
 pyrosim.End()
