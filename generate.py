b37edfacc4cd2fcc788fe14f94d2f53d02d9bc6a
Alvaro Bolaños <alvarobolanos@MacBook-Air-270.local>
2024-07-27 23:16:37 -0600

    move generation to functions add generate_robot.

--- generate.py
+++ generate.py
@@ -1,16 +1,33 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("world.sdf")
+def create_world():
+    pyrosim.Start_SDF("world.sdf")
 
-#Dimensions of the box
-length = 1
-width = 1
-height = 1
-#Position of the box
-x = 0
-y = 0
-z = 0.5
+    #Dimensions of the box
+    length = 1
+    width = 1
+    height = 1
+    #Position of the box
+    x = 0
+    y = 0
+    z = 0.5
 
-pyrosim.Send_Cube(name = "box", pos = [x, y, z], size = [length, width, height])
+    pyrosim.Send_Cube(name = "box", pos = [x, y, z], size = [length, width, height])
+    pyrosim.End()
 
-pyrosim.End()
+def create_robot():
+    pyrosim.Start_URDF("body.urdf")
+    #Dimensions of the box
+    length = 1
+    width = 1
+    height = 1
+    #Position of the box
+    x = 0
+    y = 0
+    z = 0.5
+
+    pyrosim.Send_Cube(name = "torso", pos = [x, y, z], size = [length, width, height])
+    pyrosim.End()
+
+create_world()
+create_robot()
