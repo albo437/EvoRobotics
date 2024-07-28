afae9250d2adda33ad82e29fb601172573e285ff
Alvaro Bolaños <alvarobolanos@MacBook-Air-270.local>
2024-07-28 00:34:04 -0600

    3 link 2 joint robot

--- simulate.py
+++ simulate.py
@@ -8,11 +8,10 @@ p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
-for i in range(1000):
+for i in range(500):
     time.sleep(1/60)
-    print(i) 
+    print(i)
     p.stepSimulation()
 
 p.disconnect()
