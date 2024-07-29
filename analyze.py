fb829f8110e583229875d1b921f9c729df4384f7
Alvaro Bolaños <alvarobolanos@MacBook-Air-270.local>
2024-07-29 12:40:43 -0600

    plot sensor data from robot legs.

---
+++ analyze.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+backLegSensorValues = np.load('data/backLegSensorValues.npy')
+frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
+# Plot the data
+plt.plot(backLegSensorValues, label='Back Leg')
+plt.plot(frontLegSensorValues, label='Front Leg')
+plt.legend()
+plt.show()
