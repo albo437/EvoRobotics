3908cfc2647f40e10de444160c91ae0cb3e1ebd4
Alvaro Bolaños <alvarobolanos@MacBook-Air-270.local>
2024-07-31 22:38:48 -0600

    new constants file

---
+++ constants.py
@@ -0,0 +1,8 @@
+import numpy as np
+gravity = 9.8
+BackLegAmplitude = np.pi/4
+BackLegFrequency = 1
+BackLegPhaseOffset = 0
+FrontLegAmplitude = np.pi/4
+FrontLegFrequency = 1
+FrontLegPhaseOffset = np.pi
